720ad8c506196d0257f20e68f5bb4486347762bf
mesachin486 <mesachin486@gmail.com>
2023-11-27 23:22:35 +0530

    commit 5

--- app.py
+++ app.py
@@ -58,6 +58,7 @@ def generate_comic():
     return jsonify(images)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 
 
